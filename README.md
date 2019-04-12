3ae82b8ddd3418f3f71d5af22c1130bc5fbd7c97
acosta <alexmcosta.dev@gmail.com>
2019-04-12 00:10:27 -0700

    Get output of EBS volume

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 ![alt-text](https://i.pinimg.com/originals/74/68/f1/7468f1d665e551fad8eac0c9f97977e3.jpg)
 
 ### Summary
-Narkwhal goes through your AWS account looking for old and possibly removable EBS volumes that should be removed.
+Narkwhal goes through your AWS account using the AWS SDK looking for old EBS volumes that should be removed.
+
+### Requirements
+- [AWS Credentials folder](https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#creating-the-credentials-file)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "fmt"
+
+  "github.com/aws/aws-sdk-go/service/ec2"
+  "github.com/aws/aws-sdk-go/aws/awserr"
+  "github.com/aws/aws-sdk-go/aws/session"
+  )
+
+func main() {
+
+  // First let us configure the session
+  // This session will take the default credentials found at ~/.aws/
+  sess := session.Must(session.NewSessionWithOptions(session.Options{
+    SharedConfigState: session.SharedConfigEnable,
+    }))
+
+  // Create a service with our session
+  svc := ec2.New(sess)
+
+  // Output the results of of the volumes 
+  result, err := svc.DescribeVolumes(&ec2.DescribeVolumesInput{})
+  if err != nil {
+      if aerr, ok := err.(awserr.Error); ok {
+          switch aerr.Code() {
+          default:
+              fmt.Println(aerr.Error())
+          }
+      } else {
+          // Print the error, cast err to awserr.Error to get the Code and
+          // Message from an error.
+          fmt.Println(err.Error())
+      }
+      return
+  }
+
+  fmt.Println(result)
+}
