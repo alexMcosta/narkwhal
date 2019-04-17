50deaba2fc26a4ff96d110beb6f1e497c0e92435
acosta <alexmcosta.dev@gmail.com>
2019-04-17 10:47:30 -0700

    Change function name of grabAvailableVolumes since it is no exported and does not return only IDs

--- ingest/ingest.go
+++ ingest/ingest.go
@@ -20,8 +20,8 @@ func createSession(accountFlag string, regionFlag string) *ec2.EC2 {
 	return svc
 }
 
-// GrabAvailableVolumeIDs Uses the AWS SDK to search for all available volumes in the current region
-func GrabAvailableVolumesIDs(accountFlag string, regionFlag string) (volume *ec2.DescribeVolumesOutput) {
+// grabAvailableVolumes Uses the AWS SDK to search for all available volumes in the current region
+func grabAvailableVolumes(accountFlag string, regionFlag string) (volume *ec2.DescribeVolumesOutput) {
 
 	svc := createSession(accountFlag, regionFlag)
 	// Let us filter for all available EBS volumes
@@ -63,7 +63,7 @@ func GrabAvailableVolumesIDs(accountFlag string, regionFlag string) (volume *ec2
 
 // ListVolumeIDs will list the volume IDs that are about to be deleted
 func ListVolumeIDs(accountFlag string, regionFlag string) {
-	input := GrabAvailableVolumesIDs(accountFlag, regionFlag)
+	input := grabAvailableVolumes(accountFlag, regionFlag)
 
 	if input.Volumes == nil {
 		fmt.Println("~~~~~~~~~~~~~~~~~~()~~~~")
@@ -81,7 +81,7 @@ func ListVolumeIDs(accountFlag string, regionFlag string) {
 // RemoveAvailableEBS Removes all avail able EBS volumes based on the current default region
 func RemoveAvailableEBS(accountFlag string, regionFlag string) {
 
-	input := GrabAvailableVolumesIDs(accountFlag, regionFlag)
+	input := grabAvailableVolumes(accountFlag, regionFlag)
 
 	for _, value := range input.Volumes {
 
