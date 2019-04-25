7f1b22d16b947233108e721df5eaf1eaaf0109c7
acosta <alexmcosta.dev@gmail.com>
2019-04-25 06:33:02 -0700

    Add AM, EU, SP region based grouping look up

--- pkg/process/MultiRegion.go
+++ pkg/process/MultiRegion.go
@@ -30,7 +30,40 @@ func MultiRegion(regions string) []string {
 			"sa-east-1",
 		}
 		return sliceOfRegions
+	}
 
+	if regions == "AM" {
+		sliceOfRegions = []string{
+			"us-east-1",
+			"us-east-2",
+			"us-west-1",
+			"us-west-2",
+			"ca-central-1",
+			"sa-east-1",
+		}
+		return sliceOfRegions
+	}
+
+	if regions == "EU" {
+		sliceOfRegions = []string{
+			"eu-central-1",
+			"eu-west-1",
+			"eu-west-2",
+			"eu-west-3",
+			"eu-north-1",
+		}
+		return sliceOfRegions
+	}
+
+	if regions == "AP" {
+		sliceOfRegions = []string{
+			"ap-south-1",
+			"ap-northeast-1",
+			"ap-northeast-2",
+			"ap-southeast-1",
+			"ap-southeast-2",
+		}
+		return sliceOfRegions
 	}
 
 	if strings.Contains(regions, ",") {
